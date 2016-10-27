a90b3b59a46792cfa2651cfa26b6c86aac39630f
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-27 12:38:37 +0100

    adds power saving mode + assoc. tests

--- jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -6,27 +6,58 @@ describe('thermostat: ',function(){
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees',function(){
-    thermostat = new Thermostat()
-    expect(thermostat.tempReader()).toEqual(20)
-  })
+    it('starts at 20 degrees',function(){
+      expect(thermostat.tempReader()).toEqual(20)
+    })
 
-    describe('temperature change: ', function(){
-
-      beforeEach(function(){
-        thermostat._temperature = 20
-      })
-
-      it('can increase temperature',function(){
-        thermostat.up(5)
-        expect(thermostat.tempReader()).toEqual(25)
-      })
-
-      it('can decrease temperature',function(){
-        thermostat.down(2)
-        expect(thermostat.tempReader()).toEqual(18)
-      })
-
+    it('expects power saving mode to be on', function() {
+      expect(thermostat.isSavingPower()).toEqual(true)
     });
+
+
+  describe('temperature change: ', function(){
+
+    beforeEach(function(){
+      thermostat._temperature = 20
+    })
+
+    it('can increase temperature',function(){
+      thermostat.up(5)
+      expect(thermostat.tempReader()).toEqual(25)
+    })
+
+    it('can decrease temperature',function(){
+      thermostat.down(2)
+      expect(thermostat.tempReader()).toEqual(18)
+    })
+
+    it('prevents temperature going below 10',function(){
+      expect(function(){
+        thermostat.down(11)
+      }).toThrowError(
+    'Minimum temperature is 10- temperature set to 10.')
+    });
+
+    it('sets temperature to 10 when user attempts to set it lower than 10',function() {
+     try { thermostat.down(400) }
+     catch(Error) {
+        expect(thermostat.tempReader()).toEqual(10)
+      }
+    })
+
+    it('limits max temp to 25 if power saving is on',function() {
+      expect(function() {
+        thermostat.up(6)
+      }).toThrowError('Power saving mode is active: max temperature is 25. Temperature set to 25.')
+    });
+
+    it('limits max temp to 32 if power saving mode is off', function() {
+      thermostat.
+      expect(function() {
+        thermostat.up(13)
+      }).toThrowError('Max temperature is 32- temperature set to 32')
+    });
+
+  });
 
 })
--- jasmine-standalone-2.5.2/src/thermostat.js
+++ jasmine-standalone-2.5.2/src/thermostat.js
@@ -8,9 +8,28 @@ function Thermostat() {
 };
 
 Thermostat.prototype.up = function(num) {
+  if(this.isSavingPower() && this._temperature + num > 25) {
+    this._temperature = 25;
+    throw new Error("Power saving mode is active: max temperature is 25. Temperature set to 25.")
+  };
+  if(this._temperature + num > 32) {
+    this._temperature = 32;
+    throw new Error('Max temperature is 32- temperature set to 32')
+  };
   this._temperature += num;
 };
 
 Thermostat.prototype.down = function (num) {
+  if(this.tempReader() - num < 10) {
+    this._temperature = 10;
+    throw new Error('Minimum temperature is 10- temperature set to 10.')
+  };
   this._temperature -= num;
+};
+
+
+
+Thermostat.prototype.isSavingPower = function () {
+
+  return true;
 };
