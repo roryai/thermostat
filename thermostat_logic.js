23ca5f67dd9d2705294f80b58a3ed87a4a4009d7
lcbeh <lichien2010@yahoo.co.uk>
2016-10-28 14:59:55 +0100

    Rory-li error catch

--- thermostat_logic.js
+++ thermostat_logic.js
@@ -5,7 +5,17 @@ $( "#up" ).click(function() {
 });
 
 // DOWN BUTTON CHANGES NUMBER ON DISPLAY
-
+$( "#down" ).click(function() {
+  thermostat.down(40);
+  // this code allows line 14 to run when integer on line 9 is too large
+  if(!Error) {
+  try {thermostat.down(1)}
+  catch(Error) {
+    $("#display_text").html(thermostat.tempReader());
+  }
+}
+$("#display_text").html(thermostat.tempReader());
+});
 
 // CODE TO PUT CHECKBOX ON PAGE
 $("[name='my-checkbox']").bootstrapSwitch();
