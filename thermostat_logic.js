6d7ebbc4efb91b08683a2901bfd4793295ffc4cf
lcbeh <lichien2010@yahoo.co.uk>
2016-10-28 15:44:07 +0100

    Trying to get bootstrap switch to work

--- thermostat_logic.js
+++ thermostat_logic.js
@@ -1,4 +1,6 @@
 // UP BUTTON CHANGES NUMBER ON DISPLAY
+var thermostat = new Thermostat();
+
 $( "#up" ).click(function() {
   thermostat.up(1);
   $("#display_text").html(thermostat.tempReader());
@@ -6,16 +8,25 @@ $( "#up" ).click(function() {
 
 // DOWN BUTTON CHANGES NUMBER ON DISPLAY
 $( "#down" ).click(function() {
-  thermostat.down(40);
-  // this code allows line 14 to run when integer on line 9 is too large
-  if(!Error) {
-  try {thermostat.down(1)}
-  catch(Error) {
-    $("#display_text").html(thermostat.tempReader());
-  }
-}
-$("#display_text").html(thermostat.tempReader());
+  thermostat.down(1);
+  $("#display_text").html(thermostat.tempReader());
 });
 
-// CODE TO PUT CHECKBOX ON PAGE
-$("[name='my-checkbox']").bootstrapSwitch();
+$('input[name="my-checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
+  console.log(this); // DOM element
+  console.log(event); // jQuery event
+  console.log(state); // true | false
+});
+CODE TO PUT CHECKBOX ON PAGE
+$("[name='my-checkbox']").bootstrapSwitch('state', true, false);
+console.log(state)
+$('#on-off').click(function() {
+  console.log(state);
+  thermostat.changePowerMode();
+  console.log(state);
+  if (thermostat._powerSavingMode == true) {
+    thermostat._powerSavingMode = false;
+  } else {
+    thermostat._powerSavingMode = true;
+  }
+});
